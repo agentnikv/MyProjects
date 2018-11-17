45a9f057ec8d0fd7914abe2ad9a948b49adfad58
Nikhil Verghese <agentnikv@gmail.com>
2018-11-17 12:09:04 -0500

    Stalker Project

--- Udemy_JavaScript/script.js
+++ Udemy_JavaScript/script.js
@@ -1,3 +1,5 @@
-var userName = prompt("What is your name?");
-alert("Nice to meet you, " + userName);
-console.log("Also great to meet you, " + userName);
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+alert("Nice to meet you, " + firstName + " " + lastName);
+var age = prompt("What is your age name?");
+alert("You are, " + age);
