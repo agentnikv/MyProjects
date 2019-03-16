cf0e562f661e8adbdb54d2f44e49b4a3c02aa9c8
Nikhil Verghese <agentnikv@gmail.com>
2019-03-16 11:48:04 -0400

    EVen

--- Udemy_JavaScript/EVEn.js
+++ Udemy_JavaScript/EVEn.js
@@ -8,3 +8,20 @@ function isEven(x) {
 }
 
 isEven(6)
+
+
+
+
+
+
+function factorial(x) {
+
+    var y=1
+	for (var i = x; i > 0; i=i-1) {
+		y=y*i;
+		console.log("y= " + y)
+	}
+	return y
+}
+
+factorial(5)
